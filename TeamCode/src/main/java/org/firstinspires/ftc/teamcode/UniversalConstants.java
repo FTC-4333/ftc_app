101514b3657939fd7f11b3c12633ae6816598cb8
SaltyChicken <nano0117@gmail.com>
2018-12-16 21:43:55 -0500

    attempted to use a servo, asked ethan, the original writer of the code, on discord about how I could use a servo in his code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalConstants.java
@@ -1,18 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-/*
-This code is written as an example only.
-Obviously, it was not tested on your team's robot.
-Teams who use and reference this code are expected to understand code they use.
-If you use our code and see us at competition, come say hello!
-*/
-
 public class UniversalConstants {
-    static final String LEFT1NAME = "l1";
-    static final String LEFT2NAME = "l2";
-    static final String RIGHT1NAME = "r1";
-    static final String RIGHT2NAME = "r2";
-    static final String TESTSERVO = "ts";
+    static final String LEFT1NAME      = "l1";
+    static final String LEFT2NAME      = "l2";
+    static final String RIGHT1NAME     = "r1";
+    static final String RIGHT2NAME     = "r2";
+    static final String LEFTCOLLECTOR  = "lc";
+    static final String RIGHTCOLLECTOR = "rc";
+
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rover_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rover_v1.java
@@ -2,84 +2,89 @@
 ADB guide can be found at:
 https://ftcprogramming.wordpress.com/2015/11/30/building-ftc_app-wirelessly/
 */
-package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-
+package org.firstinspires.ftc.teamcode                ;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode ;
+import com.qualcomm.robotcore.hardware.DcMotor        ;
+import com.qualcomm.robotcore.hardware.DcMotorSimple  ;
+import com.qualcomm.robotcore.hardware.Servo          ;
 import java.util.Arrays;
 
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="rover_v1", group="TeleOp")
+public class rover_v1 extends OpMode
+{
 
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="revmecanum", group="TeleOp")
-public class revmecanum extends OpMode {
-
-    private static final double TRIGGERTHRESHOLD = .2;
+    private static final double TRIGGERTHRESHOLD = .2     ;
     private static final double ACCEPTINPUTTHRESHOLD = .15;
     private static final double SCALEDPOWER = 0.5; //Emphasis on current controller reading (vs current motor power) on the drive train
 
     private static DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel;
-    private static Servo testservo;
-    @Override
-    public void init() {
-        leftFrontWheel = hardwareMap.dcMotor.get(UniversalConstants.LEFT1NAME);
-        leftBackWheel = hardwareMap.dcMotor.get(UniversalConstants.LEFT2NAME);
-        rightFrontWheel = hardwareMap.dcMotor.get(UniversalConstants.RIGHT1NAME);
-        rightBackWheel = hardwareMap.dcMotor.get(UniversalConstants.RIGHT2NAME);
+    private static Servo leftCollector, rightCollector;
 
-        //reverse all
+    @Override
+    public void init()
+    {
+
+        leftFrontWheel  = hardwareMap.dcMotor.get(UniversalConstants.LEFT1NAME) ;
+        leftBackWheel   = hardwareMap.dcMotor.get(UniversalConstants.LEFT2NAME) ;
+        rightFrontWheel = hardwareMap.dcMotor.get(UniversalConstants.RIGHT1NAME);
+        rightBackWheel  = hardwareMap.dcMotor.get(UniversalConstants.RIGHT2NAME);
+        leftCollector  = hardwareMap.servo.get(UniversalConstants.LEFTCOLLECTOR) ;
+        rightCollector = hardwareMap.servo.get(UniversalConstants.RIGHTCOLLECTOR);
+
+        //reverse all but rightFrontWheel, because of the way that the REV motors are oriented
         leftFrontWheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBackWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBackWheel.setDirection(DcMotorSimple.Direction.REVERSE) ;
         rightBackWheel.setDirection(DcMotorSimple.Direction.REVERSE);
         //rightFrontWheel.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        servo = hardwareMap.servo.get(UniversalConstants.TESTSERVO);
 
         double volts = hardwareMap.voltageSensor.get("Expansion Hub 2").getVoltage();
     }
 
     @Override
-    public void loop() {
-        double inputY = Math.abs(gamepad1.left_stick_y) > ACCEPTINPUTTHRESHOLD ? gamepad1.left_stick_y : 0;
+
+    public void loop()
+    {
+        double inputY = Math.abs(gamepad1.left_stick_y) > ACCEPTINPUTTHRESHOLD ? gamepad1.left_stick_y : 0 ;
         double inputX = Math.abs(gamepad1.left_stick_x) > ACCEPTINPUTTHRESHOLD ? -gamepad1.left_stick_x : 0;
         double inputC = Math.abs(gamepad1.right_stick_x)> ACCEPTINPUTTHRESHOLD ? -gamepad1.right_stick_x: 0;
 
-
+        double inputL = (gamepad2.right_trigger);
+        double inputR = (gamepad2.right_trigger);
 
         arcadeMecanum(inputY, inputX, inputC, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+      //  collector(inputL, inputR, leftCollector, rightCollector);
     }
 
     // y - forwards
     // x - side
     // c - rotation
-    public static void arcadeMecanum(double y, double x, double c, DcMotor leftFront, DcMotor rightFront, DcMotor leftBack, DcMotor rightBack) {
+    public static void arcadeMecanum(double y, double x, double c, DcMotor leftFront, DcMotor rightFront, DcMotor leftBack, DcMotor rightBack)
+    {
         //leftFront and rightBack are reversed
         double leftFrontVal  = y + x + c;       /*r2*/
         double rightFrontVal = y - x - c;       /*r1*/
         double leftBackVal   = y - x + c;       /*l2*/
         double rightBackVal  = y + x - c;       /*l1*/
 
-        double leftZuccVal  = 0;
-
-
-
         //Move range to between 0 and +1, if not already
         double[] wheelPowers = {rightFrontVal, leftFrontVal, leftBackVal, rightBackVal};
         Arrays.sort(wheelPowers);
-        if (wheelPowers[3] > 1) {
+        if (wheelPowers[3] > 1)
+        {
             leftFrontVal  /= wheelPowers[3];
             rightFrontVal /= wheelPowers[3];
             leftBackVal   /= wheelPowers[3];
             rightBackVal  /= wheelPowers[3];
         }
+
         double scaledPower = SCALEDPOWER;
 
-
-        leftFront.setPower(leftFrontVal*scaledPower+leftFront.getPower()*(1-scaledPower));
-        rightFront.setPower(rightFrontVal*scaledPower+rightFront.getPower()*(1-scaledPower));
-        leftBack.setPower(leftBackVal*scaledPower+leftBack.getPower()*(1-scaledPower));
-        rightBack.setPower(rightBackVal*scaledPower+rightBack.getPower()*(1-scaledPower));
+        leftFront.setPower(leftFrontVal*scaledPower+leftFront.getPower()*(1-scaledPower))    ;
+        rightFront.setPower(rightFrontVal*scaledPower+rightFront.getPower()*(1-scaledPower)) ;
+        leftBack.setPower(leftBackVal*scaledPower+leftBack.getPower()*(1-scaledPower))       ;
+        rightBack.setPower(rightBackVal*scaledPower+rightBack.getPower()*(1-scaledPower))    ;
 
     }
+
+   // public static void collector(double l, double r, leftCollector, rightCollector);
 }
