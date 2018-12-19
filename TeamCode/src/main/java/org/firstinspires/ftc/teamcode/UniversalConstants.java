ee9edc2842f6aeab8885c4f1d745738bdf8e3b82
SaltyChicken <nano0117@gmail.com>
2018-12-18 22:25:53 -0500

    Formatting. I also wrote code future code for the arm. I also assigned the servo control to the A and B buttons instead of the stick's y value. Still not sure how the servos work with the 0s and 1s, need to test it at the lab tomorrow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalConstants.java
@@ -9,4 +9,9 @@ public class UniversalConstants
     static final String RIGHT2NAME     = "r2";
     static final String LEFTCOLLECTOR  = "lc";
     static final String RIGHTCOLLECTOR = "rc";
+    //arm0 is the bottom part of the arm, the "shoulder"
+    //arm1 is the top part, the "forearm"
+    static final String ARM0           = "a0";
+    static final String ARM1           = "a1";
+    static final String LIFTER         = "l";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rover_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rover_v1.java
@@ -14,21 +14,26 @@ public class rover_v1 extends OpMode
 {
     private static final double TRIGGERTHRESHOLD = .2     ;
     private static final double ACCEPTINPUTTHRESHOLD = .15;
-    private static final double SCALEDPOWER = 0.5; //Emphasis on current controller reading (vs current motor power) on the drive train
+    private static final double SCALEDPOWER = 1; //Emphasis on current controller reading (vs current motor power) on the drive train
 
-    private static DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel;
+    private static DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel, arm0, arm1, lifter;
     private static Servo leftCollector, rightCollector;
 
     @Override
     public void init()
+    //this is where the code for init-ing and reversing goes
     {
         leftFrontWheel  = hardwareMap.dcMotor.get(UniversalConstants.LEFT1NAME) ;
         leftBackWheel   = hardwareMap.dcMotor.get(UniversalConstants.LEFT2NAME) ;
         rightFrontWheel = hardwareMap.dcMotor.get(UniversalConstants.RIGHT1NAME);
         rightBackWheel  = hardwareMap.dcMotor.get(UniversalConstants.RIGHT2NAME);
 
-        leftCollector   = hardwareMap.servo.get(UniversalConstants.LEFTCOLLECTOR) ;
-        rightCollector  = hardwareMap.servo.get(UniversalConstants.RIGHTCOLLECTOR);
+        arm0   = hardwareMap.dcMotor.get(UniversalConstants.ARM0);
+        arm1   = hardwareMap.dcMotor.get(UniversalConstants.ARM1);
+        lifter = hardwareMap.dcMotor.get(UniversalConstants.LIFTER);
+
+        leftCollector  = hardwareMap.servo.get(UniversalConstants.LEFTCOLLECTOR) ;
+        rightCollector = hardwareMap.servo.get(UniversalConstants.RIGHTCOLLECTOR);
 
         //reverse all but rightFrontWheel, because of the way that the REV motors are oriented
         leftFrontWheel.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -42,11 +47,35 @@ public class rover_v1 extends OpMode
     @Override
 
     public void loop()
-
+    //this is where the code for moving things goes
     {
-        leftCollector.setPosition(gamepad2.left_stick_y);
-        rightCollector.setPosition(-gamepad2.left_stick_y);
+        //use A button and B button to control the intake servos
+        //A sucks, B spits
+        if (gamepad2.a)
+        {
+            leftCollector.setPosition(1);
+            rightCollector.setPosition(-1);
+        }
+        else if (gamepad2.b)
+        {
+            rightCollector.setPosition(-1);
+            leftCollector.setPosition(1);
+        }
 
+        else
+            {
+                rightCollector.setPosition(0);
+                leftCollector.setPosition(0);
+            }
+
+        //control arm using left and right stick y values
+        //left stick controls arm0, the "shoulder" or bottom part
+        //right stick controls arm1, the "forearm" or top part
+        arm0.setPower(gamepad2.left_stick_y);
+        arm1.setPower(gamepad2.right_stick_y);
+
+        //moves mecanum wheel motors based on absolute values from the sticks that take into account rotation
+        //strafing works
         double inputY = Math.abs(gamepad1.left_stick_y) > ACCEPTINPUTTHRESHOLD ? gamepad1.left_stick_y : 0 ;
         double inputX = Math.abs(gamepad1.left_stick_x) > ACCEPTINPUTTHRESHOLD ? -gamepad1.left_stick_x : 0;
         double inputC = Math.abs(gamepad1.right_stick_x)> ACCEPTINPUTTHRESHOLD ? -gamepad1.right_stick_x: 0;
@@ -59,7 +88,6 @@ public class rover_v1 extends OpMode
     // c - rotation
     public static void arcadeMecanum(double y, double x, double c, DcMotor leftFront, DcMotor rightFront, DcMotor leftBack, DcMotor rightBack)
     {
-        //leftFront and rightBack are reversed
         double leftFrontVal  = y + x + c;       /*r2*/
         double rightFrontVal = y - x - c;       /*r1*/
         double leftBackVal   = y - x + c;       /*l2*/
