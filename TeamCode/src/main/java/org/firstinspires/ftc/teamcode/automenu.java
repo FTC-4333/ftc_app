4f8c29c8d45a3f4e16fa9d916eab585e131f2139
x230\Lisandro <nano0117@gmail.com>
2018-12-28 16:56:43 -0500

    forgot to stage some files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automenu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automenu.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
-public class automenu {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode ;
+import com.qualcomm.robotcore.hardware.DcMotor        ;
+import com.qualcomm.robotcore.hardware.DcMotorSimple  ;
+import com.qualcomm.robotcore.hardware.Servo          ;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="automenu", group="Autonomous")
+public class automenu extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // initialize robot
+        boolean lastDown = false, lastUp = false;
+        int waitTime = 0;
+        while (!opModeIsActive()) {
+            if (gamepad1.dpad_down && !lastDown) {
+                // We only want to increment if we pressed the button on this loop.
+                // If the button was pressed lastTime (lastDown is true) we don't want to add one more to waitTime.
+                // This would make waitTime go up very quickly.
+                waitTime++;
+            } else if (gamepad1.dpad_up && !lastUp) {
+                if (waitTime > 0) {
+                    // If waitTime already equals 0, waitTime-- would make it negative.
+                    // We can't sleep for a negative amount of time, so we don't let the user make waitTime < 0;
+                    waitTime--;
+                }
+            }
+            telemetry.addData("Will wait for: ", waitTime + " seconds");
+            telemetry.update();
+            lastDown = gamepad1.dpad_down;
+            lastUp = gamepad1.dpad_up;
+        }
+        // the loop can't end until the "Play" button is pressed
+
+        sleep(waitTime * 1000);
+
+        // do other things
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autorover_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autorover_v1.java
@@ -13,10 +13,6 @@ import java.util.Arrays;
 @Autonomous(name="autorover_v1", group="Autonomous")
 public class autorover_v1 extends OpMode
 {
-    private static final double TRIGGERTHRESHOLD = .2     ;
-    private static final double ACCEPTINPUTTHRESHOLD = .15;
-    private static final double SCALEDPOWER = 1; //Emphasis on current controller reading (vs current motor power) on the drive train
-
     private static DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel, arm0, arm1, lifter;
     private static Servo leftCollector, rightCollector;
 
@@ -50,70 +46,12 @@ public class autorover_v1 extends OpMode
     public void loop()
     //this is where the code for moving things goes
     {
-        //use A button and B button to control the intake servos
-        //A sucks, B spits
-        //servos accept values from 0 to 1, 0.5 makes the servo "stop"
-        if (gamepad2.a)
-        {
-            //this makes the intake servos "suck" when a button is presses
-            leftCollector.setPosition(1);
-            rightCollector.setPosition(-1);
-        }
-        else if (gamepad2.b)
-        {
-            //this makes the intake servos "spit" when b button is pressed
-            leftCollector.setPosition(-1);
-            rightCollector.setPosition(1);
-        }
 
-        else
-        {
-            //this "stops" the servos, but they actually drift a bit
-            leftCollector.setPosition(0.5);
-            rightCollector.setPosition(0.5);
-        }
 
-        //control arm using left and right stick y values
-        //left stick controls arm0, the "shoulder" or bottom part
-        //right stick controls arm1, the "forearm" or top part
-        arm0.setPower(gamepad2.left_stick_y);
-        arm1.setPower(gamepad2.right_stick_y);
 
-        //moves mecanum wheel motors based on absolute values from the sticks that take into account rotation
-        //strafing works
-        double inputY = Math.abs(gamepad1.left_stick_y) > ACCEPTINPUTTHRESHOLD ? gamepad1.left_stick_y : 0 ;
-        double inputX = Math.abs(gamepad1.left_stick_x) > ACCEPTINPUTTHRESHOLD ? -gamepad1.left_stick_x : 0;
-        double inputC = Math.abs(gamepad1.right_stick_x)> ACCEPTINPUTTHRESHOLD ? -gamepad1.right_stick_x: 0;
 
-        arcadeMecanum(inputY, inputX, inputC, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
-    }
 
-    // y - forwards
-    // x - side
-    // c - rotation
-    public static void arcadeMecanum(double y, double x, double c, DcMotor leftFront, DcMotor rightFront, DcMotor leftBack, DcMotor rightBack)
-    {
-        double leftFrontVal  = y + x + c;       /*r2*/
-        double rightFrontVal = y - x - c;       /*r1*/
-        double leftBackVal   = y - x + c;       /*l2*/
-        double rightBackVal  = y + x - c;       /*l1*/
 
-        //Move range to between 0 and +1, if not already
-        double[] wheelPowers = {rightFrontVal, leftFrontVal, leftBackVal, rightBackVal};
-        Arrays.sort(wheelPowers);
-        if (wheelPowers[3] > 1)
-        {
-            leftFrontVal  /= wheelPowers[3];
-            rightFrontVal /= wheelPowers[3];
-            leftBackVal   /= wheelPowers[3];
-            rightBackVal  /= wheelPowers[3];
-        }
-
-        double scaledPower = SCALEDPOWER;
-
-        leftFront.setPower(leftFrontVal*scaledPower+leftFront.getPower()*(1-scaledPower))    ;
-        rightFront.setPower(rightFrontVal*scaledPower+rightFront.getPower()*(1-scaledPower)) ;
-        leftBack.setPower(leftBackVal*scaledPower+leftBack.getPower()*(1-scaledPower))       ;
-        rightBack.setPower(rightBackVal*scaledPower+rightBack.getPower()*(1-scaledPower))    ;
+        
     }
 }
